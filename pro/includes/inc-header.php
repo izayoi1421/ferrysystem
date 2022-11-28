f20b5d8b8c9e77b51ee87de0a66afbac0a7616f5
ElijahDesuuu <70250126+ElijahDesuuu@users.noreply.github.com>
2022-11-28 22:49:53 +0800

    first commit

---
+++ pro/includes/inc-header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<?php
+require_once '../constants.php';
+
+?>
+
+<head>
+    <link rel="icon" href="https://i.postimg.cc/sg41YRkg/boat.jpg">
+    <title>FERRY HEHE</title>
+    <link href="assets/css/jquery-ui.css" rel="stylesheet" type="text/csss" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
---
+++ pro/pay.php
@@ -0,0 +1,50 @@
+<?php
+  require_once 'session.php';
+  require_once '../constants.php';
+  if (!isset($_SESSION['amount'], $_SESSION['email'])) {
+    @session_destroy();
+    header("Location: ../");
+    exit;
+  }
+
+
+
+  $pay = curl_init();
+  $email = $_SESSION['email'];
+  $amount = $_SESSION['amount'] . "00";
+  // die($amount);
+  //the amount in kobo. This value is actually NGN 5000
+  curl_setopt_array($pay, array(
+    CURLOPT_URL => "https://api.paystack.co/transaction/initialize",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_CUSTOMREQUEST => "POST",
+
+    CURLOPT_POSTFIELDS => json_encode([
+      'amount' => $amount,
+      'email' => $email,
+    ]),
+    CURLOPT_HTTPHEADER => [
+      "authorization: Bearer sk_test_2a720be0e29e2c82b202875f1acb4c5ca7c90340", //replace this with your own test key
+      "content-type: application/json",
+      "cache-control: no-cache"
+    ],
+  ));
+  
+
+  $response = curl_exec($pay);
+  $err = curl_error($pay);
+  if ($err) {
+    header("Location: individual.php?page=pay&error=payment&access=0");
+    exit();
+  }
+  $tranx = json_decode($response);
+  if (!$tranx->status or empty($tranx->status)) {
+    // there was an error from the API
+    header("Location: individual.php?page=pay&error=payment&access=1");
+    exit();
+  }
+
+  // redirect to page so User can pay
+  // uncomment this line to allow the user redirect to the payment page
+  header('Location: ' . $tranx->data->authorization_url);
